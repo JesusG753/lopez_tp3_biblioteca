be8b191b8d4ed825d07eb6630547c8ba4ed381c7
Jesus <2223050507@untels.edu.pe>
2025-04-08 15:30:51 -0500

    modelo listo

---
+++ modelo/Libro.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Libro{
+	private String título;
+	public void setTítulo(String título){
+		
+	}
+	public String getTítulo(){
+		
+	}
+}	
--- modelo/Persona.java
+++ modelo/Persona.java
@@ -3,9 +3,9 @@ package modelo;
 public class Persona{
 	private String nombre;
 	public void setNombre(String nombre){
-		xxxxxxxxx
+		
 	}
 	public String getNombre(){
-		yyyyyyyy
+		
 	}
 }	
